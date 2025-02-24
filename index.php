312495de6c2b231ee476c98a69cf022346a2300b
Francis Gonzales <fgonzalestello91@gmail.com>
2025-02-24 15:05:52 +0000

    gpt

--- index.php
+++ index.php
@@ -1,7 +1,10 @@
 <?php
 
 error_reporting(E_ALL);
-ini_set('display_errors',1);
+ini_set('display_errors', 1);
+ini_set('upload_max_filesize', '30M');
+ini_set('post_max_size', '30M');
+
 
 // OpenAI API Key
 $openai_api_key = str_replace("\n", "", file_get_contents(__DIR__ . '/apiKey.txt'));
